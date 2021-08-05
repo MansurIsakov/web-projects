b946b08a8ced743e11da2b622742af42018bba5e
Mansur Isakov <72330622+MansurIsakov@users.noreply.github.com>
2021-08-05 21:22:08 +0500

    Initial Commit

---
+++ vue-practice/covidTracker/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header></Header>
+  <div class="container mx-auto">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ vue-practice/covidTracker/src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <main v-if="!loading">
+    <data-title :text="title" :dataDate="dataDate"></data-title>
+
+    <data-boxes :stats="stats"></data-boxes>
+
+    <country-select
+      @get-country="getCountryData"
+      :countries="countries"
+    ></country-select>
+
+    <button
+      v-if="stats.Country"
+      @click="clearCountryData"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" alt="Loading..." class="w-24 m-auto" />
+  </main>
+</template>
+
+<script>
+import DataTitle from "@/components/DataTitle.vue";
+import DataBoxes from "@/components/DataBoxes.vue";
+import CountrySelect from "@/components/CountrySelect.vue";
+
+export default {
+  name: "Home",
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
+</script>
